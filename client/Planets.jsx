1424a48a9908933a5ca427e300b7fabd0fed9ba9
mgandolfi7 <michaelgandolfi7@gmail.com>
2023-04-05 10:15:13 -0500

    add planets component

--- client/Planets.jsx
+++ client/Planets.jsx
@@ -1,15 +1,63 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
-const Planets = () =>{
+const Planets = () => {
 
+  const [planets, setPlanets] = useState([]);
+  const [planetPicture, setPlanetPicture] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isPlanet,eq,true')
+      .then((data) => {
+        setPlanets(data.data.bodies);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
+  function getPicture(event) {
+    axios.get(`https://planets-17f2.onrender.com/planets/getPlanet?name=${event}`)
+      .then((data) => {
+        setPlanetPicture(data.data.picture);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
   return (
-    <h1>Planet Stuff</h1>);
-
-
-
+    <div className="planetsContainer">
+      <div className='table'>
+        <table>
+          <tr className="headers">
+            <th>Name</th>
+            <th>Moons</th>
+            <th>Mass</th>
+            <th>Volume</th>
+            <th>Density</th>
+            <th>Gravity</th>
+          </tr>
+          {
+            planets.length === 0 ? <td>loading</td> : planets.map((planet) => (
+              <tr key={planet.englishName}>
+                <td onClick={() => getPicture(planet.englishName)} >{planet.englishName}</td>
+                <td>{planet.moons === null ? 0 : planet.moons.length }</td>
+                <td>{planet.mass.massValue}</td>
+                <td>{planet.vol.volValue}</td>
+                <td>{planet.density}</td>
+                <td>{planet.gravity}</td>
+              </tr>
+            ))
+          }
+        </table>
+      </div>
+      <div className='photo'>
+        <img src={planetPicture}/>
+      </div>
+    </div>
+  );
 };
-
 
 export default Planets;
