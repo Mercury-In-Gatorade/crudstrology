cba348b2e777006890f9b2bcb90ddf3bac6724ad
bsegelke <bbsegelke@gmail.com>
2023-04-05 19:36:16 -0400

    working aura functionality

--- client/Aura.jsx
+++ client/Aura.jsx
@@ -2,18 +2,34 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Upload from './Upload.jsx';
 import RingLoader from 'react-spinners/RingLoader';
+import {Howl, Howler} from 'howler';
 import { auraData } from '../database/auraData.js';
 const Aura = () =>{
   //set the state to that image url
   const [image, setImage ] = useState(null);
   const [prediction, setPrediction ] = useState(null);
   const [aura, setAura ] = useState(null);
+  const [buttonClick, setButtonClick ] = useState(false);
+
+  const audio = new Audio('./audio/magic.mp3');
+
+
+ 
+  
+ 
+
+
   //get the image from cloudinary
   const getImage = ()=>{
     axios.get('/img/allimages')
       .then((response)=>{
+       
         setImage(response.data[0].url);
         setAura(randomAura);
+        audio.play();
+      }).then(()=>{
+        setButtonClick(true);
+        getPrediction();
       });
   };
 
@@ -37,9 +53,7 @@ const Aura = () =>{
       <div>
         <h1>Aura Time</h1>
         <Upload getImage={getImage}/>
-      
         <button style={{color: 'black'}} onClick={getImage}>See yo Self</button>
-        <button style={{color: 'black'}} onClick={getPrediction}>What Does It Mean</button>
         <div style={{position: 'relative'}}>
           <img src='https://i.imgur.com/LCN604d.png' style={{width: '100%'}} />
           <img src={image} style={{width: '70%', height: '65%', position: 'absolute', top: '49%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1, boxShadow: '20px 20px 20px 20pxpx rgba(0,0,0,0.5) inset',
@@ -59,22 +73,23 @@ const Aura = () =>{
       <div>
         <h1>Aura Time</h1>
         <Upload getImage={getImage}/>
-      
-        <button style={{color: 'black'}} onClick={getImage}>See yo Self</button>
-        <button style={{color: 'black'}} onClick={getPrediction}>What Does It Mean</button>
-        <div style={{position: 'relative'}}>
-          <img src='https://i.imgur.com/LCN604d.png' style={{width: '100%'}} />
-          <div className='Loading' style={{width: '70%', height: '65%', position: 'absolute', top: '70%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-              <RingLoader color="#d7b7"/>
-              <p style={{textAlign: 'center'}}>The Spirits Are Analyzing You...</p>
+        <button style={{color: 'black'}} onClick={getImage}>See Thyself</button>
+        { buttonClick ? 
+          <><div style={{ position: 'relative' }}>
+            <img src='https://i.imgur.com/LCN604d.png' style={{ width: '100%' }} />
+            <div className='Loading' style={{ width: '70%', height: '65%', position: 'absolute', top: '70%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <RingLoader color="#d7b7" />
+                <p style={{ textAlign: 'center' }}>The Spirits Are Analyzing You...</p>
+              </div>
             </div>
-          </div>
-        </div>
-        <div>
-          <h1>What It All Means</h1>
-          <p>{prediction}</p>
-        </div>
+          </div><div>
+            <h1>What It All Means</h1>
+            <p>{prediction}</p>
+          </div></>
+          : null
+        }
+       
       </div>
     );
   }
