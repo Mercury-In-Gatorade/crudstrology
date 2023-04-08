daa2cae5676be2665c8273094c2de9e809a1976a
mgandolfi7 <michaelgandolfi7@gmail.com>
2023-04-08 09:41:25 -0500

    add background color and center title for planets component

--- client/Planets.jsx
+++ client/Planets.jsx
@@ -37,7 +37,7 @@ const Planets = () => {
 
   return (
     <div className='container'>
-      <h2>Click on a planet's name to get a picture and description</h2>
+      <h2 className='title'>Click on a planet's name to get a picture and description</h2>
       <div className="planetsContainer">
         <div className='table'>
           <table>
